1c5b1280417d87482ca8f69ba1925e1b3035a59c
yousef <nayo21da@student.ju.se>
2023-05-22 20:20:11 +0200

    Fixed scaling
    Fixed the situation where window would get out of phone screen

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,7 @@
-document.querySelector("#gameWindow").style.transform =
-  "scale(" + window.innerHeight / 800 + ")";
+let scale = window.innerHeight / 800;
+
+let scaledWidth = 600 * scale;
+
+if (scaledWidth > window.innerWidth) scale = window.innerWidth / scaledWidth;
+
+document.querySelector("#gameWindow").style.transform = "scale(" + scale + ")";
