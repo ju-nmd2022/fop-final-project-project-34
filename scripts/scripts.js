f6a336e7522bea19e820ae5d3f5a9ba2d5713b21
yousef <nayo21da@student.ju.se>
2023-05-22 19:46:08 +0200

    garit khareto

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,2 @@
-// document.querySelector("#gameWindow").style.transform = "scale(" + (window.innerHeight / 800 - 0.05) + ")";
+document.querySelector("#gameWindow").style.transform =
+  "scale(" + window.innerHeight / 800 + ")";
